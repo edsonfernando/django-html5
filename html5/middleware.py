349e9974270f35004e9ed9d140a102d03e396897
Marinho Brandão <marinho@gmail.com>
2010-05-19 15:14:40 -0300

    Improving middleware to support forced HTML5 choice

--- html5/middleware.py
+++ html5/middleware.py
@@ -23,29 +23,44 @@ class HTML5Middleware(object):
     
     Only browsers with 70% or more set True to that attribute."""
 
+    force_html5 = None
+
     def process_request(self, request):
         # Default is False
         request.supports_html5 = False
 
-        # Google Chrome 3.0 or higher
-        m = EXP_CHROME.match(request.META['HTTP_USER_AGENT'])
-        if m and int(m.group(1)) >= 3:
+        # Checks GET param
+        if request.GET.get('force_html5', False) == '1':
+            self.force_html5 = True
+            request.supports_html5 = True
+        elif request.GET.get('force_html5', False) == '0':
+            self.force_html5 = False
+            request.supports_html5 = False
+
+        # Checks cookie
+        if not request.supports_html5 and request.COOKIES.get('force_html5', None):
             request.supports_html5 = True
 
-        # Mozilla Firefox 3.5 or higher
-        m = EXP_FIREFOX.match(request.META['HTTP_USER_AGENT'])
-        if m and float(m.group(1)) >= 3.5:
-            request.supports_html5 = True
+        if not request.supports_html5:
+            # Google Chrome 3.0 or higher
+            m = EXP_CHROME.match(request.META['HTTP_USER_AGENT'])
+            if m and int(m.group(1)) >= 3:
+                request.supports_html5 = True
 
-        # Apple Safari 4 or higher
-        m = EXP_SAFARI.match(request.META['HTTP_USER_AGENT'])
-        if m and float(m.group(1)) >= 4:
-            request.supports_html5 = True
+            # Mozilla Firefox 3.5 or higher
+            m = EXP_FIREFOX.match(request.META['HTTP_USER_AGENT'])
+            if m and float(m.group(1)) >= 3.5:
+                request.supports_html5 = True
 
-        # Opera or higher
-        m = EXP_OPERA.match(request.META['HTTP_USER_AGENT'])
-        if m and float(m.group(1)) >= 10:
-            request.supports_html5 = True
+            # Apple Safari 4 or higher
+            m = EXP_SAFARI.match(request.META['HTTP_USER_AGENT'])
+            if m and float(m.group(1)) >= 4:
+                request.supports_html5 = True
+
+            # Opera or higher
+            m = EXP_OPERA.match(request.META['HTTP_USER_AGENT'])
+            if m and float(m.group(1)) >= 10:
+                request.supports_html5 = True
 
         # Stores in thread locals
         _thread_locals.supports_html5 = request.supports_html5
@@ -54,4 +69,13 @@ class HTML5Middleware(object):
         m = EXP_IE.match(request.META['HTTP_USER_AGENT'])
         request.is_ie = bool(m)
         request.is_bad_ie = bool(m and float(m.group(1)) < 8)
+
+    def process_response(self, request, response):
+        # Stores force HTML5 or force old version
+        if self.force_html5 == True:
+            response.set_cookie('force_html5', '1')
+        elif self.force_html5 == False:
+            response.delete_cookie('force_html5')
+
+        return response
 
